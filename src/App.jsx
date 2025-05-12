b823e8bfcd6b4e7ef281fc2e6609042cbd46947c
Vidita Singh <vidita1812@gmail.com>
2025-05-13 01:13:36 +0530

    add bg color

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   return (
     <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
       
-      <div className="flex flex-wrap min-h-screen items-center">
+      <div className="flex flex-wrap min-h-screen items-center  bg-[#E6E6FA]">
         <div className="w-full">
             <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
               <ThemeButton/>
